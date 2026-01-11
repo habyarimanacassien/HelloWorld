398be70f4cd139f9b9c00fc4b967b141e3ad0e96
habyarimanacassien <c.habyarimana@ur.ac.rw>
2026-01-11 11:31:34 +0200

    hello world initialization

---
+++ hello.r
@@ -0,0 +1,8 @@
+cat("\nHello World!\n")
+cat("\nBelow I can show my first step with hypothesis test in R\n")
+cat("\nintegrated with VSCode:\n")
+x = c(2, 5, 10, 11)
+y = c(10, 4, 5, 14)
+result = t.test(x, y)
+
+print(result)
