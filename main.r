67b5db80682003344e509c956f60eade4a64912b
habyarimanacassien <c.habyarimana@ur.ac.rw>
2026-01-11 13:47:19 +0200

    improved code

---
+++ main.r
@@ -0,0 +1,8 @@
+cat("\nHello World!\n")
+cat("\nBelow I can show my first step with hypothesis test in R")
+cat("integrated with VSCode:")
+x = c(2, 5, 10, 11)
+y = c(10, 4, 5, 14)
+result = t.test(x, y)
+
+print(result)
